7852d4469624fc88ec6089cd74450759a2a9ca0d
mamadou7-source <mamadoubarrysagna7@gmail.com>
2026-06-20 16:29:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==========================
 // 1. INITIALISATION CARTE
 // ==========================
-const map = L.map('map', { zoomControl: false }).setView([12.75, -16.2667], 10);
+const map = L.map('map', { zoomControl: false }).setView([12.75, -16.2667], 12);
 L.control.zoom({ position: 'bottomleft' }).addTo(map);
 
 L.control.locate({
